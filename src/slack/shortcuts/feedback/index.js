62df06015dd92e89a84b002fa203680d2f3820f8
pikure <nihalk2@hotmail.com>
2021-03-03 17:50:55 +0530

    feat: shortcut for feedback

---
+++ src/slack/shortcuts/feedback/index.js
@@ -0,0 +1,26 @@
+const { openModal } = require("../../api");
+const { createSubmitAFeedbackTemplate } = require("../../templates");
+const {
+  VIEW_SUBMISSIONS: { FEEDBACK }
+} = require("../../../global/constants");
+const logger = require("../../../global/logger");
+
+const processFeedbackShortcut = async (payload) => {
+  try {
+    logger.debug("processFeedbackShortcut");
+
+    const {
+      team: { id: teamId },
+      user: { username },
+      trigger_id
+    } = payload;
+
+    const viewTemplate = createSubmitAFeedbackTemplate(username, FEEDBACK);
+
+    await openModal(teamId, trigger_id, viewTemplate);
+  } catch (error) {
+    logger.error("processFeedbackShortcut() -> error : ", error);
+  }
+};
+
+module.exports = { processFeedbackShortcut };
