22da28b6aafe60aefd67c728f46865c28f275b16
pikure <nihalk2@hotmail.com>
2021-11-26 00:41:30 +0530

    feat

--- src/slack/view-submissions/add-new-interest/index.js
+++ src/slack/view-submissions/add-new-interest/index.js
@@ -1,20 +1,24 @@
 const { nanoid } = require("nanoid");
 const TopicsModel = require("../../../mongo/models/Topics");
 const InterestsModel = require("../../../mongo/models/Interests");
+const { updateModal } = require("../../api");
+const {
+  createInterestsTemplate
+} = require("../../commands/interests/template");
 const {
   BLOCK_IDS: { NEW_INTEREST },
-  ACTION_IDS: { NEW_INTEREST_VALUE }
+  ACTION_IDS: { NEW_INTEREST_VALUE },
+  VIEW_SUBMISSIONS: { INTERESTS }
 } = require("../../../global/constants");
 const logger = require("../../../global/logger");
 
 const processAddNewInterest = async (payload) => {
   try {
-    logger.info("processAddNewInterest");
-
     const {
       user: { id: userId },
       team: { id: teamId },
-      view: { state }
+      view: { id: viewId, state },
+      hash
     } = payload;
 
     const newInterest = state.values[NEW_INTEREST][NEW_INTEREST_VALUE].value;
@@ -23,16 +27,19 @@ const processAddNewInterest = async (payload) => {
     const topic = await TopicsModel.findOne({ teamId });
     const interest = await InterestsModel.findOne({ teamId, userId });
 
+    let updatedTopic = null;
+    let updatedInterest = null;
+
     if (topic) {
       // update topics for team
       const { topics } = topic;
 
       topics.push({ id: topicId, value: newInterest });
 
-      await TopicsModel.updateOne({ teamId }, { $set: { topics } });
+      updatedTopic = await TopicsModel.findOneAndUpdate({ teamId }, { topics });
     } else {
       // create new topic record for team
-      await new TopicsModel({
+      updatedTopic = await new TopicsModel({
         teamId,
         topics: [{ id: topicId, value: newInterest }]
       }).save();
@@ -44,20 +51,44 @@ const processAddNewInterest = async (payload) => {
 
       interests.push({ id: topicId, value: newInterest });
 
-      await InterestsModel.updateOne(
+      updatedInterest = await InterestsModel.findOneAndUpdate(
         { teamId, userId },
-        { $set: { interests } }
+        { interests }
       );
     } else {
       // create new interest record for user
-      await new InterestsModel({
+      updatedInterest = await new InterestsModel({
         teamId,
         userId,
         interests: [{ id: topicId, value: newInterest }]
       }).save();
     }
 
-    logger.debug("newInterest : ", newInterest);
+    if (updatedTopic && updatedInterest) {
+      const { topics: updatedTopics } = updatedTopic;
+
+      const { interests: updatedInterests } = updatedInterest;
+
+      const unSelectedTopics = [];
+
+      updatedTopics.forEach((elem) => {
+        if (!updatedInterests.some((item) => item.id === elem.id)) {
+          unSelectedTopics.push(elem);
+        }
+      });
+
+      await updateModal({
+        teamId,
+        viewId,
+        hash,
+        view: createInterestsTemplate(
+          INTERESTS,
+          updatedTopics,
+          unSelectedTopics,
+          updatedInterests
+        )
+      });
+    }
   } catch (error) {
     logger.error("processAddNewInterest() -> error : ", error);
   }
