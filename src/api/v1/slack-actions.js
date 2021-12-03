f41a3f04a9efd6d88aefa80b4b1c0fea83faa557
pikure <nihalk2@hotmail.com>
2021-12-03 23:27:23 +0530

    fix

--- src/api/v1/slack-actions.js
+++ src/api/v1/slack-actions.js
@@ -52,8 +52,6 @@ router.post("/", async (req, res) => {
       if (response.success) {
         return res.status(200).send({});
       }
-
-      return res.sendStatus(200);
     }
 
     if (type === SHORTCUT) {
--- src/slack/view-submissions/start-icebreaker-question/index.js
+++ src/slack/view-submissions/start-icebreaker-question/index.js
@@ -4,7 +4,7 @@ const {
   SLACK_ERROR: { CHANNEL_NOT_FOUND },
 } = require("../../../global/constants");
 const IceBreakerQuestionsModel = require("../../../mongo/models/IceBreakerQuestions");
-const { slackPostMessageToChannel, updateModal } = require("../../api");
+const { slackPostMessageToChannel, pushViewToModal } = require("../../api");
 const { createIcebreakerQuestionSubmittedTemplate } = require("./template");
 const { createNotInChannelTemplate } = require("../../templates");
 const logger = require("../../../global/logger");
@@ -12,6 +12,7 @@ const logger = require("../../../global/logger");
 const processStartIcebreakerQuestion = async payload => {
   try {
     const {
+      trigger_id,
       user: { id: userId },
       team: { id: teamId },
       view: { id: viewId, hash, state },
@@ -36,14 +37,14 @@ const processStartIcebreakerQuestion = async payload => {
     );
 
     if (response && !response.ok && response.error === CHANNEL_NOT_FOUND) {
-      await updateModal({
-        teamId,
-        viewId,
-        hash,
-        view: createNotInChannelTemplate(),
-      });
+      // await updateModal({
+      //   teamId,
+      //   viewId,
+      //   hash,
+      //   view: createNotInChannelTemplate(),
+      // });
 
-      return { success: false };
+      await pushViewToModal(teamId, trigger_id, createNotInChannelTemplate());
     }
 
     return { success: true };
