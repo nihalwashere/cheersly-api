d73651f59c6c716298fb441a918a8f2051ca6583
pikure <nihalk2@hotmail.com>
2021-12-23 19:38:17 +0530

    script

--- scripts/notifyCustomers.js
+++ scripts/notifyCustomers.js
@@ -1,74 +1,173 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const pMap = require("p-map");
-const { getAllAuths } = require("../src/mongo/helper/auth");
-const { postMessageToHook } = require("../src/slack/api");
+const AuthModel = require("../src/mongo/models/Auth");
+const UserModel = require("../src/mongo/models/User");
+const { slackPostMessageToChannel } = require("../src/slack/api");
+const { UserRoles } = require("../src/enums/userRoles");
+const {
+  getAppUrl,
+  getAppHomeLink,
+  waitForMilliSeconds,
+} = require("../src/utils/common");
 const logger = require("../src/global/logger");
-
-// const teamIds = ["TT4G2QY1W", "T01PE2U3AKG"]; // prod
-// const teamIds = ["TUDG6C681"]; // prod internal
-// const teamIds = ["T018LGG2CTY"]; // dev
 
 const service = async () => {
   try {
     logger.info("STARTING NOTIFY CUSTOMERS SCRIPT");
 
-    const auths = await getAllAuths();
+    const auths = await AuthModel.find({
+      slackDeleted: false,
+      // "slackInstallation.team.id": "",
+    });
 
     const handler = async auth => {
       const {
         slackInstallation: {
-          team: { id: teamId },
+          authed_user: { id: authed_user_id },
+          team: { id: team_id },
         },
       } = auth;
 
-      await postMessageToHook(teamId, [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text:
-              "Hey there! :wave: \n\n This is to inform that we are taking *Cheersly* off from the Slack app directory and will be manually distributing it (not through the Slack app directory).",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text:
-              "*Why are we moving off from the Slack app directory?* \n Whenever we submit any changes to the app, we need the changes to get approved by the Slack team, this often takes too long which we cannot afford at the moment. We are moving fast and committed to solving the problems of our customers.",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text:
-              "*How does this impact you?* \n Your current installation of Cheersly will no longer work. But the good news is that you can use Cheersly by joining our private beta program. We're prioritizing people ops managers at fast-growing startups that use Slack. If you're a fit, you'll get access to Cheersly and help us improve remote team connection. Just send an email to support@cheersly.club with your interest and we will take it forward from there.",
-          },
-        },
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: "Cheers :beers:",
-          },
-        },
-        {
-          type: "context",
-          elements: [
-            {
+      const users = [authed_user_id];
+
+      const admins = await UserModel.find({
+        "slackUserData.team_id": team_id,
+        role: UserRoles.ADMIN,
+      });
+
+      admins.map(admin => {
+        if (!users.includes(admin.slackUserData.id)) {
+          users.push(admin.slackUserData.id);
+        }
+      });
+
+      for (let i = 0; i < users.length; i += 1) {
+        await slackPostMessageToChannel(users[i], team_id, [
+          {
+            type: "section",
+            text: {
               type: "mrkdwn",
-              text: "Need help? contact support@cheersly.club",
+              text: "Hey there :wave:! Hope you are doing good!",
             },
-          ],
-        },
-      ]);
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "Christmas is just round the corner and we are here to share some gifts! YAAS! You heard that right, GIFTS! :gift:",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "We are your :santa: and very excited to say that we have launched new features for *Cheersly*.",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `Head over to the ${getAppHomeLink(
+                team_id
+              )} tab of Cheersly to,`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                ":point_right: Appreciate your peers by saying cheers :beers:",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: ":point_right: Ask anything by starting a poll :bar_chart:",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                ":point_right: Share feedback with your team and get heard :speech_balloon:",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: ":point_right: Play fun Icebreaker games :ice_cube:",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                ":point_right: Play Tic Tac Toe :x: :o: or Stone Paper Scissors :v: with your co-worker",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `Deliver a fun, candid and social recognition & rewards experience for your employees. Recognize when employees align with your company values to reinforce good behavior. You can manage rewards and company values from the <${getAppUrl()}|app dashboard>.`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Introduce *Cheersly* to the team!",
+            },
+            accessory: {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Introduce to team",
+                emoji: true,
+              },
+              action_id: "INTRODUCE_TO_TEAM",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Excited right? Yes! We are too!",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                "We would be launching pricing plans for *Cheersly* starting from *15th Jan,2022*. You have three more weeks to try out Cheersly! Make the most of it. We are all ears and would love to hear back from you. Please feel free to write to us at support@cheersly.club",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Happy Holidays! :beers:",
+            },
+          },
+        ]);
+
+        await waitForMilliSeconds(1000);
+      }
     };
 
     await pMap(auths, handler, { concurrency: 1 });
   } catch (error) {
-    logger.error("POLLS CRON SERVICE FAILED -> error : ", error);
+    logger.error("NOTIFY CUSTOMERS SCRIPT ERROR : ", error);
   }
 };
 
